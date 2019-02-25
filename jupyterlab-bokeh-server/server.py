57537e977f8face7d2ed02e9db3e755933f072fa
Matthew Rocklin <mrocklin@gmail.com>
2019-02-25 13:44:20 -0800

    use datetime axis

--- jupyterlab-bokeh-server/server.py
+++ jupyterlab-bokeh-server/server.py
@@ -2,25 +2,25 @@ from bokeh.server.server import Server
 from bokeh.plotting import figure, ColumnDataSource
 
 import random
+import time
 
 
 def lineplot(doc):
-    fig = figure(title="Line plot!", sizing_mode="stretch_both")
+    fig = figure(title="Line plot!", sizing_mode="stretch_both", x_axis_type="datetime")
     source = ColumnDataSource({"x": [], "y": []})
     fig.line(source=source, x="x", y="y")
 
     doc.title = "Hello, world!"
     doc.add_root(fig)
 
-    old_x = 0
-    old_y = 0
+    y = 0
 
     def cb():
-        nonlocal old_x, old_y
-        old_x += 1
-        old_y += random.random() - 0.5
+        nonlocal y
+        now = time.time() * 1000  # bokeh measures in ms
+        y += random.random() - 0.5
 
-        source.stream({"x": [old_x], "y": [old_y]}, 100)
+        source.stream({"x": [now], "y": [y]}, 100)
 
     doc.add_periodic_callback(cb, 100)
 
@@ -29,9 +29,9 @@ routes = {"/": lineplot}
 
 
 if __name__ == "__main__":
+    from tornado.ioloop import IOLoop
+
     server = Server(routes, port=5000)
     server.start()
-
-    from tornado.ioloop import IOLoop
 
     IOLoop.current().start()
