dfa4a5c3b76bd3ad8d973d0a2192845f28ccebc2
Matthew Rocklin <mrocklin@gmail.com>
2019-02-25 14:36:39 -0800

    add histogram plot

--- jupyterlab_bokeh_server/server.py
+++ jupyterlab_bokeh_server/server.py
@@ -6,12 +6,13 @@ import time
 
 import sys
 
+
 def lineplot(doc):
     fig = figure(title="Line plot!", sizing_mode="stretch_both", x_axis_type="datetime")
     source = ColumnDataSource({"x": [], "y": []})
     fig.line(source=source, x="x", y="y")
 
-    doc.title = "Hello, world!"
+    doc.title = "Line Plot!"
     doc.add_root(fig)
 
     y = 0
@@ -26,12 +27,37 @@ def lineplot(doc):
     doc.add_periodic_callback(cb, 100)
 
 
-routes = {"/": lineplot}
+def histogram(doc):
+    fig = figure(title="Histogram!", sizing_mode="stretch_both")
+
+    left = [0, 1, 2, 3, 4, 5]
+    right = [l + 0.9 for l in left]
+    ys = [0 for _ in left]
+
+    source = ColumnDataSource({"left": left, "right": right, "y": [ys]})
+
+    fig.quad(source=source, left="left", right="right", bottom=0, top="y", color="blue")
+
+    doc.title = "Histogram!"
+    doc.add_root(fig)
+
+    def cb():
+        nonlocal ys
+
+        for i, y in enumerate(ys):
+            ys[i] = 0.9 * y + 0.1 * random.random()
+
+        source.data.update({"y": ys})
+
+    doc.add_periodic_callback(cb, 100)
+
+
+routes = {"/line": lineplot, "/histogram": histogram}
 
 if __name__ == "__main__":
     from tornado.ioloop import IOLoop
 
-    server = Server(routes, port=int(sys.argv[1]), allow_websocket_origin=['*'])
+    server = Server(routes, port=int(sys.argv[1]), allow_websocket_origin=["*"])
     server.start()
 
     IOLoop.current().start()
