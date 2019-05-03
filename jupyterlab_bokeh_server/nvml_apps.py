c8c536a78f10a252847a16a7f41c5221a8e36979
rjzamora <rzamora@nvidia.com>
2019-05-03 14:43:20 -0700

    adding pci throughput to gpu-reosources app

--- jupyterlab_bokeh_server/nvml_apps.py
+++ jupyterlab_bokeh_server/nvml_apps.py
@@ -16,11 +16,10 @@ gpu_handles = [ pynvml.nvmlDeviceGetHandleByIndex(i) for i in range(ngpus) ]
 def gpu(doc):
     fig = figure(title="GPU Usage", sizing_mode="stretch_both", y_range=[0, 100])
 
-    gpu = [ pynvml.nvmlDeviceGetUtilizationRates( gpu_handles[i] ).gpu+1 for i in range(ngpus) ]
+    gpu = [ pynvml.nvmlDeviceGetUtilizationRates( gpu_handles[i] ).gpu for i in range(ngpus) ]
     left = list(range(len(gpu)))
     right = [l + 0.8 for l in left]
     source = ColumnDataSource({"left": left, "right": right, "gpu": gpu})
-    #colors = ['#440154', '#404387', '#29788E', '#22A784', '#79D151']
     mapper = LinearColorMapper(palette=all_palettes['RdYlBu'][4], low=0, high=100)
 
     fig.quad(
@@ -31,29 +30,29 @@ def gpu(doc):
     doc.add_root(fig)
 
     def cb():
-        source.data.update({"gpu": [ pynvml.nvmlDeviceGetUtilizationRates( gpu_handles[i] ).gpu+0.1 for i in range(ngpus) ]})
+        source.data.update({"gpu": [ pynvml.nvmlDeviceGetUtilizationRates( gpu_handles[i] ).gpu for i in range(ngpus) ]})
 
     doc.add_periodic_callback(cb, 200)
 
 def pci(doc):
 
-    tx_fig = figure(title="TX Bytes [KB/s]", sizing_mode="stretch_both", y_range=[0, 1000000])
-    pci_tx = [ pynvml.nvmlDeviceGetPcieThroughput( gpu_handles[i] , pynvml.NVML_PCIE_UTIL_TX_BYTES ) for i in range(ngpus) ]
+    tx_fig = figure(title="TX Bytes [MB/s]", sizing_mode="stretch_both", y_range=[0, 5000])
+    pci_tx = [ pynvml.nvmlDeviceGetPcieThroughput( gpu_handles[i] , pynvml.NVML_PCIE_UTIL_TX_BYTES )/1024 for i in range(ngpus) ]
     left = list(range(len(pci_tx)))
     right = [l + 0.8 for l in left]
     source = ColumnDataSource({"left": left, "right": right, "pci-tx": pci_tx})
-    mapper = LinearColorMapper(palette=all_palettes['RdYlBu'][4], low=0, high=1000000)
+    mapper = LinearColorMapper(palette=all_palettes['RdYlBu'][4], low=0, high=5000)
 
     tx_fig.quad(
         source=source, left="left", right="right", bottom=0, top="pci-tx", color={"field": "pci-tx", "transform": mapper}
     )
 
-    rx_fig = figure(title="RX Bytes [KB/s]", sizing_mode="stretch_both", y_range=[0, 1000000])
-    pci_rx = [ pynvml.nvmlDeviceGetPcieThroughput( gpu_handles[i] , pynvml.NVML_PCIE_UTIL_RX_BYTES ) for i in range(ngpus) ]
+    rx_fig = figure(title="RX Bytes [MB/s]", sizing_mode="stretch_both", y_range=[0, 5000])
+    pci_rx = [ pynvml.nvmlDeviceGetPcieThroughput( gpu_handles[i] , pynvml.NVML_PCIE_UTIL_RX_BYTES )/1024 for i in range(ngpus) ]
     left = list(range(len(pci_rx)))
     right = [l + 0.8 for l in left]
     source = ColumnDataSource({"left": left, "right": right, "pci-rx": pci_rx})
-    mapper = LinearColorMapper(palette=all_palettes['RdYlBu'][4], low=0, high=1000000)
+    mapper = LinearColorMapper(palette=all_palettes['RdYlBu'][4], low=0, high=5000)
 
     rx_fig.quad(
         source=source, left="left", right="right", bottom=0, top="pci-rx", color={"field": "pci-rx", "transform": mapper}
@@ -66,8 +65,8 @@ def pci(doc):
 
     def cb():
         src_dict = {}
-        src_dict["pci-rx"] = [ pynvml.nvmlDeviceGetPcieThroughput( gpu_handles[i] , pynvml.NVML_PCIE_UTIL_TX_BYTES ) for i in range(ngpus) ]
-        src_dict["pci-tx"] = [ pynvml.nvmlDeviceGetPcieThroughput( gpu_handles[i] , pynvml.NVML_PCIE_UTIL_RX_BYTES ) for i in range(ngpus) ]
+        src_dict["pci-tx"] = [ pynvml.nvmlDeviceGetPcieThroughput( gpu_handles[i] , pynvml.NVML_PCIE_UTIL_TX_BYTES )/1024 for i in range(ngpus) ]
+        src_dict["pci-rx"] = [ pynvml.nvmlDeviceGetPcieThroughput( gpu_handles[i] , pynvml.NVML_PCIE_UTIL_RX_BYTES )/1024 for i in range(ngpus) ]
         source.data.update(src_dict)
 
     doc.add_periodic_callback(cb, 200)
@@ -83,7 +82,8 @@ def gpu_resource_timeline(doc):
     y_range = DataRange1d(follow="end", follow_interval=20000, range_padding=0)
     tools = "reset,xpan,xwheel_zoom"
 
-    item_dict = {"time": [], "gpu-total": [], "memory-total": []}
+    item_dict = {"time": [], "gpu-total": [], "memory-total": [], \
+                 "rx-total": [], "tx-total": []}
     for i in range(ngpus):
         item_dict["gpu-"+str(i)] = []
         item_dict["memory-"+str(i)] = []
@@ -95,7 +95,7 @@ def gpu_resource_timeline(doc):
         return color_list[ ind % len(color_list) ]
 
     memory_fig = figure(
-        title="Memory Utilization",
+        title="Memory Utilization (per Device)",
         sizing_mode="stretch_both",
         x_axis_type="datetime",
         y_range=[0, gpu_mem_max],
@@ -103,12 +103,12 @@ def gpu_resource_timeline(doc):
         tools=tools,
     )
     for i in range(ngpus):
-        memory_fig.line(source=source, x="time", y="memory-"+str(i), color=_get_color(i), legend="GPU-"+str(i))
+        memory_fig.line(source=source, x="time", y="memory-"+str(i), color=_get_color(i))#, legend="GPU-"+str(i))
     memory_fig.yaxis.formatter = NumeralTickFormatter(format="0.0b")
-    memory_fig.legend.location = "top_left"
+    #memory_fig.legend.location = "top_left"
 
     gpu_fig = figure(
-        title="GPU Utilization [%]",
+        title="GPU Utilization (per Device) [%]",
         sizing_mode="stretch_both",
         x_axis_type="datetime",
         y_range=[0, 100],
@@ -116,8 +116,8 @@ def gpu_resource_timeline(doc):
         tools=tools,
     )
     for i in range(ngpus):
-        gpu_fig.line(source=source, x="time", y="gpu-"+str(i), color=_get_color(i), legend="GPU-"+str(i))
-    gpu_fig.legend.location = "top_left"
+        gpu_fig.line(source=source, x="time", y="gpu-"+str(i), color=_get_color(i))#, legend="GPU-"+str(i))
+    #gpu_fig.legend.location = "top_left"
 
     tot_fig = figure(
         title="Total Utilization [%]",
@@ -131,9 +131,21 @@ def gpu_resource_timeline(doc):
     tot_fig.line(source=source, x="time", y="memory-total", color="red", legend="Total-Memory")
     tot_fig.legend.location = "top_left"
 
+    pci_fig = figure(
+        title="Total PCI Throughput [MB/s]",
+        sizing_mode="stretch_both",
+        x_axis_type="datetime",
+        y_range=[0, 10000],
+        x_range=x_range,
+        tools=tools,
+    )
+    pci_fig.line(source=source, x="time", y="tx-total", color="blue", legend="TX")
+    pci_fig.line(source=source, x="time", y="rx-total", color="red", legend="RX")
+    pci_fig.legend.location = "top_left"
+
     doc.title = "Resource Timeline"
     doc.add_root(
-        column(gpu_fig, memory_fig, tot_fig, sizing_mode="stretch_both")
+        column(gpu_fig, memory_fig, tot_fig, pci_fig, sizing_mode="stretch_both")
     )
 
     last_time = time.time()
@@ -144,15 +156,23 @@ def gpu_resource_timeline(doc):
         src_dict = {"time": [now * 1000]}
         gpu_tot = 0
         mem_tot = 0
+        tx_tot = 0
+        rx_tot = 0
         for i in range(ngpus):
             gpu = pynvml.nvmlDeviceGetUtilizationRates( gpu_handles[i] ).gpu
             mem = pynvml.nvmlDeviceGetMemoryInfo( gpu_handles[i] ).used
+            tx = pynvml.nvmlDeviceGetPcieThroughput( gpu_handles[i], pynvml.NVML_PCIE_UTIL_TX_BYTES )/1024
+            rx = pynvml.nvmlDeviceGetPcieThroughput( gpu_handles[i], pynvml.NVML_PCIE_UTIL_RX_BYTES )/1024
             gpu_tot += gpu
             mem_tot += mem / (1024*1024)
+            rx_tot += rx
+            tx_tot += tx
             src_dict["gpu-"+str(i)] = [gpu]
             src_dict["memory-"+str(i)] = [mem]
         src_dict["gpu-total"] = [gpu_tot / ngpus]
         src_dict["memory-total"] = [(mem_tot/gpu_mem_sum)*100]
+        src_dict["tx-total"] = [ tx_tot ]
+        src_dict["rx-total"] = [ rx_tot ]
 
         source.stream(
             src_dict,
