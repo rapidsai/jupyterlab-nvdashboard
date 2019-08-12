32d7978fe023f1458a5ef215ac040c645344b24d
Jacob Tomlinson <jtomlinson@nvidia.com>
2019-08-12 08:26:01 -0700

    Update setup

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import setuptools
 setuptools.setup(
     name="jupyterlab-nvdashboard",
     version='0.1.0',
-    url="",
+    url="https://github.com/jacobtomlinson/jupyterlab-nvdashboard",
     author="NV Dashbaord contributors",
-    description="",
+    description="A JupyterLab extension for displaying dashboards of GPU usage.",
     packages=setuptools.find_packages(),
         keywords=['Jupyter'],
         classifiers=['Framework :: Jupyter'],
