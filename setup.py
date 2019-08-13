195a5fedbf2cd8eb2221abe55831b92e0bf3da86
Jacob Tomlinson <jtomlinson@nvidia.com>
2019-08-13 04:22:23 -0700

    Add README to setup as long description

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 import setuptools
 
+# read the contents of README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="jupyterlab-nvdashboard",
     version='0.1.3',
     url="https://github.com/jacobtomlinson/jupyterlab-nvdashboard",
     author="NV Dashbaord contributors",
     description="A JupyterLab extension for displaying dashboards of GPU usage.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
         keywords=['Jupyter'],
         classifiers=['Framework :: Jupyter'],
