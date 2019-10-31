079b2be270d80a1fa5beb26751f3595f35f2dd3e
Raymond Douglass <ray@raydouglass.com>
2019-10-31 15:38:45 -0400

    FIX Typo in npmrc filename

--- ci/cpu/upload-packages.sh
+++ ci/cpu/upload-packages.sh
@@ -14,7 +14,7 @@ fi
 
 anaconda -t ${MY_UPLOAD_KEY} upload -u ${CONDA_USERNAME:-rapidsai} --label main --force "`conda build conda/recipes/jupyterlab-nvdashboard --output`"
 
-echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > .nmprc
+echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > .npmrc
 if [[ "$BUILD_MODE" == "branch" && "${SOURCE_BRANCH}" != 'master' ]]; then
   echo "Nightly build, publishing to npm with nightly tag"
   npm publish --tag=nightly
