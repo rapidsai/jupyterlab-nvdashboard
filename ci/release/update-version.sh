e7377bb7f31b83c76e763a1c9458edf2e1575afa
Raymond Douglass <ray@raydouglass.com>
2020-03-23 14:29:02 -0400

    FIX update-version.sh is now non-interactive

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -12,5 +12,34 @@ set -e
 # Grab argument for release type
 RELEASE_TYPE=$1
 
+# Get current version and calculate next versions
+CURRENT_TAG=`git tag | grep -xE 'v[0-9\.]+' | sort --version-sort | tail -n 1 | tr -d 'v'`
+CURRENT_MAJOR=`echo $CURRENT_TAG | awk '{split($0, a, "."); print a[1]}'`
+CURRENT_MINOR=`echo $CURRENT_TAG | awk '{split($0, a, "."); print a[2]}'`
+CURRENT_PATCH=`echo $CURRENT_TAG | awk '{split($0, a, "."); print a[3]}'`
+CURRENT_SHORT_TAG=${CURRENT_MAJOR}.${CURRENT_MINOR}
+NEXT_MAJOR=$((CURRENT_MAJOR + 1))
+NEXT_MINOR=$((CURRENT_MINOR + 1))
+NEXT_PATCH=$((CURRENT_PATCH + 1))
+NEXT_FULL_TAG=""
+NEXT_SHORT_TAG=""
+
+# Determine release type
+if [ "$RELEASE_TYPE" == "major" ]; then
+  NEXT_FULL_TAG="${NEXT_MAJOR}.0.0"
+  NEXT_SHORT_TAG="${NEXT_MAJOR}.0"
+elif [ "$RELEASE_TYPE" == "minor" ]; then
+  NEXT_FULL_TAG="${CURRENT_MAJOR}.${NEXT_MINOR}.0"
+  NEXT_SHORT_TAG="${CURRENT_MAJOR}.${NEXT_MINOR}"
+elif [ "$RELEASE_TYPE" == "patch" ]; then
+  NEXT_FULL_TAG="${CURRENT_MAJOR}.${CURRENT_MINOR}.${NEXT_PATCH}"
+  NEXT_SHORT_TAG="${CURRENT_MAJOR}.${CURRENT_MINOR}"
+else
+  echo "Incorrect release type; use 'major', 'minor', or 'patch' as an argument"
+  exit 1
+fi
+
+echo "Preparing '$RELEASE_TYPE' release [$CURRENT_TAG -> $NEXT_FULL_TAG]"
+
 jlpm config set version-tag-prefix ""
-jlpm version --no-git-tag-version --non-interactive --${RELEASE_TYPE}
+jlpm version --no-git-tag-version --non-interactive --${RELEASE_TYPE} --new-version=${NEXT_FULL_TAG}
