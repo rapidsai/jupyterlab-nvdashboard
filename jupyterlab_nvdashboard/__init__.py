f9aec1b8d035e52ea41d3eb756e5cf43e6107460
Ajay Thorve <athorve@nvidia.com>
2023-11-27 10:21:33 -0800

    fix naming for python package

--- jupyterlab_nvdashboard/__init__.py
+++ jupyterlab_nvdashboard/__init__.py
@@ -5,18 +5,22 @@ except ImportError:
     # in editable mode with pip. It is highly recommended to install
     # the package from a stable release or in editable mode: https://pip.pypa.io/en/stable/topics/local-project-installs/#editable-installs
     import warnings
-
-    warnings.warn("Importing 'jupyterlab-nvdashboard' outside a proper installation.")
+    warnings.warn("Importing 'jupyterlab_nvdashboard' outside a proper installation.")
     __version__ = "dev"
 from .handlers import setup_handlers
 
 
 def _jupyter_labextension_paths():
-    return [{"src": "labextension", "dest": "jupyterlab-nvdashboard"}]
+    return [{
+        "src": "labextension",
+        "dest": "jupyterlab-nvdashboard"
+    }]
 
 
 def _jupyter_server_extension_points():
-    return [{"module": "jupyterlab-nvdashboard"}]
+    return [{
+        "module": "jupyterlab_nvdashboard"
+    }]
 
 
 def _load_jupyter_server_extension(server_app):
@@ -28,5 +32,5 @@ def _load_jupyter_server_extension(server_app):
         JupyterLab application instance
     """
     setup_handlers(server_app.web_app)
-    name = "jupyterlab-nvdashboard"
+    name = "jupyterlab_nvdashboard"
     server_app.log.info(f"Registered {name} server extension")
