65423daa4de08754da008aecb71978f8a84e3596
James Lamb <jlamb@nvidia.com>
2025-06-12 15:38:08 -0500

    add shellcheck to pre-commit

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -12,7 +12,8 @@ rapids-logger "Downloading artifacts from previous jobs"
 WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="${package_name}" RAPIDS_PY_WHEEL_PURE="1" rapids-download-wheels-from-github python)
 
 # echo to expand wildcard before adding `[extra]` required for pip
-python -m pip install $(echo "${WHEELHOUSE}"/jupyterlab_nvdashboard*.whl)[test]
+python -m pip install \
+    "$(echo "${WHEELHOUSE}"/jupyterlab_nvdashboard*.whl)[test]"
 
 rapids-logger "Check GPU usage"
 nvidia-smi
