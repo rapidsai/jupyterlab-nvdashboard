25bceda31171ed96c6a6a2901a947a21f8239698
ncclementi <natyclementi@gmail.com>
2026-01-09 21:08:00 +0000

    define cudf and cuml accelerator plugins

---
+++ src/accelerators/plugins/cumlaccel.ts
@@ -0,0 +1,20 @@
+/**
+ * cuml.accel accelerator plugin definition
+ */
+
+import { IAcceleratorPlugin } from '../types';
+
+/**
+ * cuml.accel provides GPU acceleration for scikit-learn compatible machine learning.
+ * It enables GPU-accelerated ML algorithms with minimal code changes.
+ */
+export const cumlaccelPlugin: IAcceleratorPlugin = {
+  id: 'cuml-accel',
+  name: 'cuML Accelerator',
+  description: 'GPU-accelerated scikit-learn compatible ML',
+  pythonPackage: 'cuml',
+  extensionName: 'cuml.accel',
+  minimumVersion: '25.12',
+  documentation: 'https://docs.rapids.ai/api/cuml/stable/cuml-accel/'
+};
+
---
+++ src/accelerators/plugins/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Export all accelerator plugins
+ * 
+ * To add a new accelerator plugin:
+ * 1. Create a new file in this directory (e.g., newAccel.ts)
+ * 2. Define the plugin following the IAcceleratorPlugin interface
+ * 3. Export it here
+ * 4. It will automatically appear in the UI!
+ */
+
+export { cudfpandasPlugin } from './cudfpandas';
+export { cumlaccelPlugin } from './cumlaccel';
