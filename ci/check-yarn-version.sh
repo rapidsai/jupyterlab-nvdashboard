6fa1533dfd5b8d48357f82c33613fe1de409ad32
James Lamb <jlamb@nvidia.com>
2026-06-09 15:23:08 -0500

    move more 'jplm' checks into pre-commit

---
+++ ci/check-yarn-version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e -u -o pipefail
+
+# yarn version needs to be consistent between what 'jlpm' wraps and what is declared
+# in 'package.json', so automated dependency updates work as expected
+JLPM_VERSION=$(jlpm --version 2>/dev/null)
+PACKAGE_JSON_VERSION=$(jq -r '."packageManager"' < ./package.json | cut -d@ -f2)
+if [[ "${JLPM_VERSION}" != "${PACKAGE_JSON_VERSION}" ]]; then
+  echo "error: yarn version from 'jlpm --version' (${JLPM_VERSION}) and 'packageManager' field in package.json (${PACKAGE_JSON_VERSION}) do not match."
+  exit 1
+fi
