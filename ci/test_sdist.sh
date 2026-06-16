e40440bc6b0be12bf85d6a473ca9b4b5fa495463
James Lamb <jlamb@nvidia.com>
2026-06-16 14:16:23 -0500

    capture PIP_CONSTRAINT before unsetting it

--- ci/test_sdist.sh
+++ ci/test_sdist.sh
@@ -9,14 +9,19 @@ source rapids-init-pip
 rapids-logger "Downloading artifacts from previous jobs"
 DOWNLOADS_DIR=$(rapids-download-from-github "$(rapids-artifact-name wheel_python jupyterlab-nvdashboard jupyterlab-nvdashboard --pure --arch any)")
 
+RAPIDS_PIP_ARGS=(
+  -v
+  --build-constraint "${PIP_CONSTRAINT}"
+  --constraint "${PIP_CONSTRAINT}"
+)
+
 # unset PIP_CONSTRAINT (set by rapids-init-pip)... it doesn't affect builds as of pip 25.3, and
 # results in an error from 'pip wheel' when set and --build-constraint is also passed
 unset PIP_CONSTRAINT
 
 # echo to expand wildcard before adding `[extra]` required for pip
 python -m pip install \
-    --build-constraint "${PIP_CONSTRAINT}" \
-    --constraint "${PIP_CONSTRAINT}" \
+    "${RAPIDS_PIP_ARGS[@]}" \
     "$(echo "${DOWNLOADS_DIR}"/jupyterlab_nvdashboard*.tar.gz)[test]"
 
 ./ci/run_pytests.sh
